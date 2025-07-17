da859856373442b2ef4056f8105e70ffadd8669a
Umut Erdoğan <109750600+umuterdgn@users.noreply.github.com>
2025-07-17 23:46:43 +0300

    home page added db api pulling work

--- frontend/webapp/controller/Home.controller.js
+++ frontend/webapp/controller/Home.controller.js
@@ -7,26 +7,46 @@ sap.ui.define([
 
   return Controller.extend("migros.controller.Home", {
     onInit: function () {
-      var oThis = this;
+  var oController = this;
+  $.ajax({
+    url: "http://localhost:8081/api/categories", // kendi sunucun
+    method: "GET",
+    success: function (data) {
+      var oHBox = oController.byId("categoryContainer");
+      oHBox.removeAllItems();
 
-      $.ajax({
-        url: "http://localhost:8081/api/categories", 
-        method: "GET",
-        success: function (data) {
-          var oModel = new JSONModel({ categories: data });
-          oThis.getView().setModel(oModel);
-        },
-        error: function () {
-          MessageToast.show("Kategoriler yüklenemedi.");
-        }
+      data.forEach(function (category) {
+        var oVBox = new sap.m.VBox({
+          width: "120px",
+          alignItems: "Center",
+          justifyContent: "Center",
+          items: [
+            new sap.m.Button({
+              icon: "sap-icon://product", // ikon
+              width: "100px",
+              press: function () {
+                // ✅ Yönlendirme yap
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(oController);
+                oRouter.navTo("productRoute", {
+                  categoryId: category.id,
+                  categoryName: category.ad
+                });
+              }
+            }),
+            new sap.m.Text({
+              text: category.ad, // ✅ Altına ismi ekle
+              textAlign: "Center"
+            })
+          ]
+        });
+        oHBox.addItem(oVBox);
       });
     },
-
-    onCategoryPress: function (oEvent) {
-      const categoryId = oEvent.getSource().data("categoryId");
-      sap.ui.core.UIComponent.getRouterFor(this).navTo("Product", {
-        categoryId: categoryId
-      });
+    error: function (err) {
+      console.log("Kategoriler alınamadı", err);
     }
+  });
+}
+
   });
 });
